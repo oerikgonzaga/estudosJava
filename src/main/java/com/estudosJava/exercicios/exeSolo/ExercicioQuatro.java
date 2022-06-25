91396426cef103fd5e4c3c0bbbe0cdded6c02b8d
oerikgonzaga <erikgonzaga@outlook.com>
2022-06-25 08:22:55 -0300

    Iniciando exercicios 3 e 4 (Solo / Prova)

---
+++ src/main/java/com/estudosJava/exercicios/exeSolo/ExercicioQuatro.java
@@ -0,0 +1,4 @@
+package main.java.com.estudosJava.exercicios.exeSolo;
+
+public class ExercicioQuatro {
+}
---
+++ src/main/java/com/estudosJava/exercicios/exeSolo/ExercicioTres.java
@@ -0,0 +1,9 @@
+package main.java.com.estudosJava.exercicios.exeSolo;
+
+public class ExercicioTres {
+
+    public static void main(String[] args) {
+
+
+    }
+}
