dc1ccbd4631d90a3a691564bd7384c19999c54fb
oerikgonzaga <erikgonzaga@outlook.com>
2022-06-29 23:09:12 -0300

    Resolvendo Exercicio três:
     Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre quantos anos ela terá em 2050.

--- src/main/java/com/estudosJava/exercicios/exeSolo/ExercicioTres.java
+++ src/main/java/com/estudosJava/exercicios/exeSolo/ExercicioTres.java
@@ -1,9 +1,24 @@
 package main.java.com.estudosJava.exercicios.exeSolo;
 
+import java.util.Scanner;
+
 public class ExercicioTres {
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Insira seu ano de Nascimento: ");
+        int anoNascimento = input.nextInt();
+        System.out.print("Insira o ano Atual: ");
+        int anoAtual = input.nextInt();
 
+        int ano2050 = 2050;
+
+        int idadeHoje = anoAtual - anoNascimento;
+        int idade2050 = ano2050 - anoNascimento;
+
+        System.out.println();
+        System.out.println("Voce completa " + idadeHoje + " anos no Ano atual");
+        System.out.println();
+        System.out.println("Sua idade em 2050 sera " + idade2050 + " anos");
     }
 }
