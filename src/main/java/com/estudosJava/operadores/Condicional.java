51dd8d6b4ff9289ed532e8e68ce1403c10104b12
oerikgonzaga <oerikgonzaga@outlook.com>
2022-07-05 00:13:19 -0300

    Operadores condicionais Lógicos: && ( e )   /  ||  ( ou )

---
+++ src/main/java/com/estudosJava/operadores/Condicional.java
@@ -0,0 +1,20 @@
+package main.java.com.estudosJava.operadores;
+
+public class Condicional {
+
+    public static void main(String[] args) {
+
+        // Operadores condicionais retornam TRUE ou FALSE
+        // Operadores Lógicos: && ( e )   /  ||  ( ou )
+
+        int idade1 = 10;
+        int idade2 = 20;
+
+        if (idade1 > 18 && idade2 > 18){  // retorna: false
+            System.out.println("Maiores de Idade");
+        }
+        if (idade1 > 18 || idade2 > 18){ // retorna: true
+            System.out.println("Menor Acompanhado");
+        }
+    }
+}
