1f835244b45fa51e63256ef9af3168153eae7a21
oerikgonzaga <oerikgonzaga@outlook.com>
2022-07-04 22:41:53 -0300

    Criando um Array com valores e exibindo dados de uma lista com for

--- src/main/java/com/estudosJava/lacosderepeticao/ForArray.java
+++ src/main/java/com/estudosJava/lacosderepeticao/ForArray.java
@@ -4,5 +4,17 @@ public class ForArray {
 
     public static void main(String[] args) {
 
+        // obs.: A primeira posição do index é 0 (zero)
+
+        // Criando um Array com valores
+
+        int[] numeros = new int[] {3,6,9,18};
+
+        // exibindo dados de uma lista com for
+
+        for (int i = 0; i < numeros.length; i++){
+            int numero = numeros[i];
+            System.out.println(numero);
+        }
     }
 }
