6a40cb80638a96c230446476268e6d731b380608
oerikgonzaga <oerikgonzaga@outlook.com>
2022-07-05 00:39:09 -0300

    Olá Mundo!

---
+++ src/main/java/com/estudosJava/intro/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.com.estudosJava.intro;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String helloWorld = "Olá Mundo!";
+
+        System.out.println(helloWorld);
+    }
+}
