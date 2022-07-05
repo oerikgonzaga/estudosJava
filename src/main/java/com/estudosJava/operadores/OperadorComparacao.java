5fe1ad2bba25986f9955bf5ea28a9dd322a6b338
oerikgonzaga <oerikgonzaga@outlook.com>
2022-07-05 00:04:29 -0300

    Operadores de Comparacao. Igual, maior, menor, maior ou igual e menor ou igual.

---
+++ src/main/java/com/estudosJava/operadores/OperadorComparacao.java
@@ -0,0 +1,24 @@
+package main.java.com.estudosJava.operadores;
+
+public class OperadorComparacao {
+
+    public static void main(String[] args) {
+
+        // Operadores de Comparação (Retornam Boolean)
+        // == igual  /  > maior  /  < menor  /  >= maior ou igual  /  <= menor ou igual
+
+        int n1 = 10;
+        int n2 = 20;
+
+        if (n2 == n1);{
+        }
+        if (n1 > n2);{
+        }
+        if (n1 < n2);{
+        }
+        if (n1 >= n2);{
+        }
+        if (n1 <= n2);{
+        }
+    }
+}
