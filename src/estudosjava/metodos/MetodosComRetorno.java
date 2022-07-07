e65b3340c385607e11c496dc17ad6040c7e35293
oerikgonzaga <oerikgonzaga@outlook.com>
2022-07-07 01:37:04 -0300

    Adicionando comentários e implementando Scanner para obter os dados através do terminal.

--- src/estudosjava/metodos/MetodosComRetorno.java
+++ src/estudosjava/metodos/MetodosComRetorno.java
@@ -1,22 +1,31 @@
 package estudosjava.metodos;
 
+import java.util.Scanner;
+
 public class MetodosComRetorno {
 
     public static void main(String[] args) {
 
+        // Metodo com Retorno
         // Aqui eu invoco o método, para imprimi-lo no terminal
+        // Após a inserção dos dados, me é retornado o resultado IMC
 
         double resultadoIMC = calcularIMC();
         System.out.println(resultadoIMC);
     }
 
     // isolando o calculo e retornando o valor do IMC.
-
     // Aqui crio o método que será invocado lá em cima;
 
     static double calcularIMC(){
-        double peso = 76.0;
-        double altura = 1.76;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Peso: ");
+        double peso = input.nextDouble();
+
+        System.out.print("Altura: ");
+        double altura = input.nextDouble();
 
         // usando var ao invés de double;
 
